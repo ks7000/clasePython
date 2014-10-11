b23f8c096e3c547f86b252de831da11b4498f54e
Luis Carrillo <carrillo.luisgerardo.luis@gmail.com>
2014-10-10 22:20:35 -0430

    Archivo para aprender funciones

---
+++ funciones.py
@@ -0,0 +1,21 @@
+#from __future__ import print_function
+
+def mi_funcion():
+    print "Hola mundo"
+
+mi_funcion()
+
+a=mi_funcion()
+
+print a
+
+
+def nombres(nombre,apellido,edad):
+    #nombre_completo= nombre, apellido #Concatenacion como tupla
+    #nombre_completo= nombre + apellido + str(edad) #Concatenacion como cadena
+    nombre_completo="Mi nombre es %s %s y tengo %d" % (nombre, apellido, edad)
+
+    print nombre_completo
+
+
+nombres("Luis ","Carrillo ",25)
