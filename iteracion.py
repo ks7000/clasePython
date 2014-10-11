2ad097a34c409140e27962a70fa67ffd673f8a1a
Luis Carrillo <carrillo.luisgerardo.luis@gmail.com>
2014-10-10 22:22:12 -0430

    Archivo para aprender sobre iteraciones

---
+++ iteracion.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+lista=['Juan','Pedro','Luis']
+
+for item in lista:
+    print item
+
+tupla=(1,2,3)
+for t in tupla:
+    print t
+
+for anio in range(2001,2012):
+    print u"Año", str(anio)
+
